07659984aac8dfc64f5d377d16eb09816f69b7e3
Gustavo Souza <gustavo.souza.1449179@sga.pucminas.br>
2023-05-15 18:40:36 -0300

    dois primeiros exercicios simples com vetores

---
+++ Aula01/Aula01/Program.cs
@@ -0,0 +1,59 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+class Program {
+
+    static void Main(String[] args){
+
+        Console.WriteLine("Digite o tamanho do vetor");
+        int vetorTamanho = int.Parse(Console.ReadLine());
+
+        int[] vetor = new int[vetorTamanho];
+
+        for (int i=0; i<vetorTamanho; i++)
+        {
+            Console.WriteLine($" Digite o {i + 1} numero do vetor ");
+            vetor[i] = int.Parse(Console.ReadLine()) ;
+        }
+
+        int soma;
+        int qunt;
+
+        soma = SomaPares(vetor);
+        qunt = ContaImpares(vetor);
+
+        Console.WriteLine();
+
+        Console.WriteLine($"Soma dos numeros pares: {soma}");
+        Console.WriteLine($"Quantidade de numeros impares: {qunt}");
+
+        Console.ReadKey();
+
+
+        static int SomaPares(int[] vetor)
+        {
+            int acc = 0;
+            for (int i = 0; i < vetor.Length; i++) {
+                if (vetor[i] % 2 == 0) {
+                    acc += vetor[i];
+                }
+
+            }
+            return acc;
+            
+        }
+
+        static int ContaImpares(int[] vetor)
+        {
+            int acc = 0;
+            for (int i = 0; i < vetor.Length; i++)
+            {
+                if (vetor[i] % 2 != 0)
+                {
+                    acc++;
+                }
+            }
+            return acc;
+
+        }
+    }
+}
---
+++ Aula02/Aula02/Program.cs
@@ -0,0 +1,35 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        int[,] matriz = new int [5,5];
+
+        for (int i = 0; i < 5; i++)
+        {
+            for (int j = 0; j < 5; j++) 
+            {
+
+                Console.WriteLine($"Digite o numero do elemento {j + 1} da linha {i + 1}");
+                matriz[i,j] = int.Parse(Console.ReadLine());
+
+            }
+
+        }
+
+        for (int i = 0; i < 5; i++)
+        {
+            for (int j = 0; j < 5; j++)
+            {
+
+                Console.WriteLine($"Os elementos da matriz são: {matriz[i, j]}");
+        
+
+            }
+
+        }
+
+
+
+
+    }
+}
